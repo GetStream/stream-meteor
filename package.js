9509ea883fce148294c70448f30d916844bdbec3
Matthisk Heimensen <m@tthisk.nl>
2015-12-03 21:31:06 +0100

    Added changelog

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'getstream:stream-meteor',
-  version: '0.2.1',
+  version: '0.2.2',
   summary: 'Getstream.io integration package for Meteor',
   git: 'https://github.com/GetStream/stream-meteor',
   documentation: 'README.md',
