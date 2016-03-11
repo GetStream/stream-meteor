544d097a48b4be67cea905790953419353409c50
Matthisk Heimensen <m@tthisk.nl>
2016-03-11 10:13:25 +0100

    Updated version

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'getstream:stream-meteor',
-  version: '0.3.7',
+  version: '0.4.0',
   summary: 'Getstream.io integration package for Meteor',
   git: 'https://github.com/GetStream/stream-meteor',
   documentation: 'README.md',
